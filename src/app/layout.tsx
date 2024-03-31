f776f541265a1e0670f483b3e0f6a44e573b0d57
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-03-31 17:40:21 +0700

    Add page layout to arrange the page contents

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -25,7 +28,72 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
-      >{children}</body>
+      >
+        <header className={cn(
+          "h-14 w-screen mb-5",
+          "flex justify-center items-center gap-5",
+          "border-b-2 border-foreground"
+        )}>
+          {/* Left: Logo */}
+          <div className="w-64 flex items-center gap-2">
+            <Image
+              src="./logo.svg"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+
+            <h3 className="text-2xl font-bold tracking-tight">
+              GreatDay
+            </h3>
+          </div>
+
+          {/* Center: Search bar */}
+          <div className='w-[600px]'>
+            Search...
+          </div>
+
+          {/* Right: User Avatar */}
+          <div className='w-64'>
+
+          </div>
+        </header>
+
+        <div className="flex justify-center gap-5">
+          <aside className='w-64'>
+            {/* My Profile Summary */}
+
+            {/* Nav Menu */}
+            <Card>
+              <CardContent>
+                <nav className="flex flex-col">
+                  <Link href="/">Home</Link>
+                  <Link href="/notification">Notification</Link>
+                  <Link href="/messages">Messages</Link>
+                </nav>
+              </CardContent>
+            </Card>
+          </aside>
+
+          {/* Center: Feed & Post Composer */}
+          <main className="min-w-[600px]">
+            {children}
+            
+          </main>
+
+          {/* Right: Who to follow */}
+          <aside className='w-64'>
+            <Card>
+              <CardHeader>
+                <CardTitle>Who to follow</CardTitle>
+              </CardHeader>
+              <CardContent>
+                HAHAHA
+              </CardContent>
+            </Card>
+          </aside>
+        </div>
+      </body>
     </html>
   );
 }
--- src/components/_home/index.tsx
+++ src/components/_home/index.tsx
@@ -1,77 +1,10 @@
 import React from 'react'
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
-import Link from 'next/link'
-import Image from 'next/image'
-import { cn } from '@/lib/utils'
 import PostComposer from './PostComposer'
 
 const Home: React.FC = () => {
   return (
     <div>
-      <header className={cn(
-        "h-14 w-screen mb-5",
-        "flex justify-center items-center gap-5",
-        "border-b-2 border-foreground"
-      )}>
-        {/* Left: Logo */}
-        <div className="w-64 flex items-center gap-2">
-          <Image
-            src="./logo.svg"
-            width={32}
-            height={32}
-            alt="logo"
-          />
-
-          <h3 className="text-2xl font-bold tracking-tight">
-            GreatDay
-          </h3>
-        </div>
-
-        {/* Center: Search bar */}
-        <div className='w-[600px]'>
-          Search...
-        </div>
-
-        {/* Right: User Avatar */}
-        <div className='w-64'>
-
-        </div>
-      </header>
-
-      <div className="flex justify-center gap-5">
-        <aside className='w-64'>
-          {/* My Profile Summary */}
-
-          {/* Nav Menu */}
-          <Card>
-            <CardContent>
-              <nav className="flex flex-col">
-                <Link href="/">Home</Link>
-                <Link href="/notification">Notification</Link>
-                <Link href="/messages">Messages</Link>
-              </nav>
-            </CardContent>
-          </Card>
-        </aside>
-
-        {/* Center: Feed & Post Composer */}
-        <main className="min-w-[600px]">
-          <PostComposer />
-          
-        </main>
-
-        {/* Right: Who to follow */}
-        <aside className='w-64'>
-          <Card>
-            <CardHeader>
-              <CardTitle>Who to follow</CardTitle>
-            </CardHeader>
-            <CardContent>
-              HAHAHA
-            </CardContent>
-          </Card>
-        </aside>
-      </div>
+      <PostComposer />
     </div>
   )
 }
