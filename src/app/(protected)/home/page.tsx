a6e2fce84fb53aa0e45b0c31491239c75998e16a
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-04-09 21:42:04 +0700

    feat: remove not-used session checking code in Home page

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,13 +1,5 @@
 import Home from "@/components/_home";
-import { EnumCookie, EnumPagePath } from "@/types/enum";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  const token = cookies().get(EnumCookie.SESSION);
-  if (!token) {
-    redirect(EnumPagePath.LOGIN);
-  }
-
   return <Home />;
 }
